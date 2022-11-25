1425bcf4d58dfc13729288c17c9ac27b600ba73a
Isaac Levy <isaa0267@stud.kea.dk>
2022-11-25 14:02:50 +0100

    Hello

--- src/SwimmingClub/Main.java
+++ src/SwimmingClub/Main.java
@@ -9,9 +9,7 @@ public class Main {
     }
     void run() throws IOException {
         Menu menu2=new Menu("|Welcome to our Swim club|\n" +
-                "| 1- Create a new User  |\n" +
-                "| 2- Swim Discipline    |\n" +
-                "| 3- Payment            |\n", "you can choice:\n","| 1- Create a new User  |\n"+
+                "", "you can choice:\n","| 1- Create a new User  |\n"+
                 "| 2- Swim Discipline    |\n" + "| 3- Payment            |\n"
         );
         menu2.readChoice();
