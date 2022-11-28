1afc722bf375e17ebf8e9ef0a334b6cbc70857c3
weix0015 <weix0015@stud.kea.dk>
2022-11-28 14:33:02 +0100

    Isaac

--- src/SwimmingClub/TimeDelaySeconds.java
+++ src/SwimmingClub/TimeDelaySeconds.java
@@ -5,11 +5,11 @@ import java.io.PrintStream;
 public class TimeDelaySeconds {
     public static void main(String[] args) {
         int timeToWait = 0; // second
-        System.out.println("Scanning");
+        System.out.println("Wait 30 seconds and try again...");
         try {
             for (int i = 30; i >= timeToWait; i--) {
                 Thread.sleep(1000);
-                System.out.print("\r" + i + " ");
+                System.out.print("\r" + i);
             }
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
