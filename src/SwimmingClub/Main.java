9c3b8b4ef6ea86798f1fc4ad7dca768dbff05b9f
Isaac Levy <isaa0267@stud.kea.dk>
2022-11-28 12:31:46 +0100

    Hello

--- src/SwimmingClub/Main.java
+++ src/SwimmingClub/Main.java
@@ -3,19 +3,12 @@ package SwimmingClub;
 import java.io.IOException;
 
 public class Main {
-    public void main() {
-    }
+
     public static void main(String[]args) throws IOException {
         new Main().run();
 
     }
 
-    void issac() {
-        int money = 999999999;
-    }
-
-    public void hello() {
-    }
     void run() throws IOException {
         Menu menu2=new Menu("|Welcome to our Swim club|\n" +
                 "", "you can choice:\n","| 1- Create a new User  |\n"+
