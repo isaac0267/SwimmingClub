42c6a1e3d1fafbcbfc27adaa45bee9e7acc96e67
weix0015 <weix0015@stud.kea.dk>
2022-11-29 14:17:39 +0100

    Isaac

--- src/SwimmingClub/UserInfo.java
+++ src/SwimmingClub/UserInfo.java
@@ -3,5 +3,17 @@ package SwimmingClub;
 import java.util.ArrayList;
 
 public class UserInfo {
+
     ArrayList<User> userList = new ArrayList<>();
+    public void test() {
+        userList.add(new User("Isaac", "Levy", "Isaklevy97@gmail.com",
+                "81745734", "Isaa0267", "HelloWorld"));
+        userList.add(new User("Wei", "Yang", "Wei97@gmail.com", "784784334",
+                "Weix0015", "HelloWorld2"));
+        userList.add(new User("Hardy", "Akira", "Hardy97@gmail.com", "34783484734",
+                "Hardy", "HelloWorld3"));
+    }
+    public static void main(String[] args) {
+
+    }
 }
