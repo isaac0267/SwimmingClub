ad618ffb1468906b7dcb330de2689e847847c080
Isaac Levy <isaa0267@stud.kea.dk>
2022-11-29 12:04:45 +0100

    Hello

--- src/SwimmingClub/LoginUser.java
+++ src/SwimmingClub/LoginUser.java
@@ -1,11 +1,64 @@
 package SwimmingClub;
 
+import java.util.Scanner;
+
 public class LoginUser {
+    // Making attributes for userName and password, yes_or_no, scan and the last attribute is passScan.
     String userName;
     String password;
     String yes_or_no;
-    String scan;
+    String scann;
     String passScan;
+    Scanner scan = new Scanner(System.in);
+    Scanner scanner = new Scanner(System.in);
+    Scanner ask = new Scanner(System.in);
+
+    // Method for the password.
+
+    public LoginUser(String userName, String   password){
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public void setUserName() {
+        // Making the Scan.
+        // make another scanner and give it an object call it name.
+
+        String userName = scan.nextLine();
+        this.userName = userName;
+
+        Scanner name = new Scanner(System.in);
+        System.out.println("Username");
+        this.userName = name.next().toLowerCase(); // give the user the option to enter the username.
+
+    }
+    // Method for the password.
+    public void setPassword() {
+        // I will make the other Scanner. In this Scanner I will use the same logic, that I used in the username.
+        Scanner pass = new Scanner(System.in);
+        System.out.println("Password");
+        password = pass.next().toLowerCase();
+        System.out.println("You are successful logge in"); // Maybe I can make a method,just for this statement.
+
+    }
+
+    public static boolean checkPassword(String password){
+        return password.equals("myPassword");
+    }
+
+    // I will make the other Method just for the ask.
+    public void ask() {
+        System.out.println("Do you want to check this or not( yes or no)");
+        yes_or_no = ask.next().toLowerCase();
+
+    }
+
+
+
+
+
+
+
     
 
 }
--- src/SwimmingClub/Main.java
+++ src/SwimmingClub/Main.java
@@ -1,8 +1,13 @@
 package SwimmingClub;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
+
+    static Scanner scan = new Scanner(System.in);
+    static LoginUser user;
+
     void run () throws IOException {
         // Menu menu = new Menu();
         // menu.readerChoice();
@@ -14,8 +19,45 @@ public class Main {
         jj.events(createList);
         jj.printJunior(createList);
     }
+
+    public static void createUserMenu(){
+        System.out.println("enter username");
+        String userName = scan.nextLine();
+        System.out.println("enter password");
+        String password = scan.nextLine();
+        user = new LoginUser(userName, password);
+    }
+
+    public static void loginMenu(){
+        System.out.println("Do you want to login?");
+        System.out.println("enter user");
+        String name = scan.nextLine();
+        System.out.println("enter password");
+        String pass = scan.nextLine();
+        if (LoginUser.checkPassword(pass)){
+            System.out.println("You are now logged in");
+        }
+        else{
+            System.out.println("wrong info");
+        }
+
+    }
+
     public static void main(String[] args) throws IOException {
+
+        createUserMenu();
+        loginMenu();
+
         new Main().run();
+
+
+
+
+
+
+
+
+
 
     }
 }
